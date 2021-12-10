c81ab2367f9c2128f7f3096b3de74aded9cdde46
Neymartri <Khoaleo2002@gmail.com>
2021-12-11 07:43:29 +1000

    rsetup-common-file

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,18 @@
-import styles from './app.module.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className={styles.app}>
-      <h1>Squid Note</h1>
-    </div>
-  );
-}
+// import styles from './app.module.css';
+
+const App = () =>{ 
+  return <div>App</div>
+} 
+
+// function App() {
+//   return (
+//     <div className={styles.app}>
+//       <h1>Squid Note</h1>
+//     </div>
+//   );
+// }
 
 export default App;
