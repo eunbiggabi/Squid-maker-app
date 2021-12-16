ea142f348486ff55f6fe715e0b12c097cd62e8e4
eunbiggabi <eunbiggabi84@gmail.com>
2021-12-16 16:55:48 +1000

    completed maker component

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,84 @@
-import styles from './app.module.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Header from './components/header/navbar/navbar'
-import Home from './pages/home/home'
-import Maker from './pages/maker/maker'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/header/navbar/navbar";
+import Home from "./pages/home/home";
+import Maker from "./pages/maker/maker";
+import styles from "./app.module.css";
 
 function App() {
+  const [cards, setCards] = useState([
+    {
+      id: "1",
+      name: "Kyu",
+      company: "Samsung",
+      theme: "light",
+      title: "Software Engineer",
+      email: "kyu@email.com",
+      message: "go for it",
+      fileName: "kyu",
+      fileURL: "kyu.png",
+    },
+    {
+      id: "2",
+      name: "Kyu2",
+      company: "Samsung",
+      theme: "dark",
+      title: "Software Engineer",
+      email: "kyu@email.com",
+      message: "go for it",
+      fileName: "kyu",
+      fileURL: "kyu.png",
+    },
+    {
+      id: "3",
+      name: "Kyu3",
+      company: "Samsung",
+      theme: "colorful",
+      title: "Software Engineer",
+      email: "kyu@email.com",
+      message: "go for it",
+      fileName: "kyu",
+      fileURL: null,
+    },
+  ]);
+
+  const addCard = (card) => {
+    const updated = [...cards, card];
+    setCards(updated);
+  };
+
+  const updateCard = (card) => {
+    const updated = cards.map((item) => {
+      if (card.id === item.id) {
+        return card;
+      }
+      return item;
+    });
+    setCards(updated);
+  };
+
+  const deleteCard = (card) => {
+    const updated = cards.filter((item) => item.id !== card.id);
+    setCards(updated);
+  };
+
   return (
     <div className={styles.app}>
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/home" element={<Home />}/>
-          <Route path="/maker" element={<Maker />}/>
+          <Route path="/home" element={<Home />} />
+          <Route
+            path="/maker"
+            element={
+              <Maker
+                cards={cards}
+                addCard={addCard}
+                updateCard={updateCard}
+                deleteCard={deleteCard}
+              />
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/maker/maker.jsx
+++ src/pages/maker/maker.jsx
@@ -3,63 +3,8 @@ import Editor from "./editor/editor";
 import Preview from "./preview/preview";
 import styles from "./maker.module.css";
 
-export default function Maker() {
-  const [cards, setCards] = useState([
-    {
-      id: "1",
-      name: "Kyu",
-      company: "Samsung",
-      theme: "light",
-      title: "Software Engineer",
-      email: "kyu@email.com",
-      message: "go for it",
-      fileName: "kyu",
-      fileURL: "kyu.png",
-    },
-    {
-      id: "2",
-      name: "Kyu2",
-      company: "Samsung",
-      theme: "dark",
-      title: "Software Engineer",
-      email: "kyu@email.com",
-      message: "go for it",
-      fileName: "kyu",
-      fileURL: "kyu.png",
-    },
-    {
-      id: "3",
-      name: "Kyu3",
-      company: "Samsung",
-      theme: "colorful",
-      title: "Software Engineer",
-      email: "kyu@email.com",
-      message: "go for it",
-      fileName: "kyu",
-      fileURL: null,
-    },
-  ]);
-
-  const addCard = (card) => {
-    const updated = [...cards, card];
-    setCards(updated);
-  };
-
-  const updateCard = (card) => {
-    const updated = cards.map((item) => {
-      if (card.id === item.id) {
-        return card;
-      }
-      return item;
-    });
-    setCards(updated);
-  };
-
-  const deleteCard = (card) => {
-    const updated = cards.filter((item) => item.id !== card.id);
-    setCards(updated);
-  };
-
+export default function Maker({cards, addCard, updateCard, deleteCard}) {
+  
   return (
     <section className={styles.maker}>
       <div className={styles.container}>
