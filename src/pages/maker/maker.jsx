2a6e029adf732b3f16e5b00537b155798b990f95
eunbiggabi <eunbiggabi84@gmail.com>
2021-12-16 16:17:35 +1000

    handle state change on editor

--- src/pages/maker/maker.jsx
+++ src/pages/maker/maker.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react"
-import Editor from "./editor/editor"
-import Preview from "./preview/preview"
-import styles from "./maker.module.css"
+import { useState } from "react";
+import Editor from "./editor/editor";
+import Preview from "./preview/preview";
+import styles from "./maker.module.css";
 
 export default function Maker() {
   const [cards, setCards] = useState([
@@ -14,7 +14,7 @@ export default function Maker() {
       email: "kyu@email.com",
       message: "go for it",
       fileName: "kyu",
-      fileURL: "kyu.png"
+      fileURL: "kyu.png",
     },
     {
       id: "2",
@@ -25,7 +25,7 @@ export default function Maker() {
       email: "kyu@email.com",
       message: "go for it",
       fileName: "kyu",
-      fileURL: "kyu.png"
+      fileURL: "kyu.png",
     },
     {
       id: "3",
@@ -36,21 +36,41 @@ export default function Maker() {
       email: "kyu@email.com",
       message: "go for it",
       fileName: "kyu",
-      fileURL: null
-    }
-  ])
+      fileURL: null,
+    },
+  ]);
 
-  const addCard = card => {
-    const updated = [...cards, card]
+  const addCard = (card) => {
+    const updated = [...cards, card];
     setCards(updated);
-  }
+  };
+
+  const updateCard = (card) => {
+    const updated = cards.map((item) => {
+      if (card.id === item.id) {
+        return card;
+      }
+      return item;
+    });
+    setCards(updated);
+  };
+
+  const deleteCard = (card) => {
+    const updated = cards.filter((item) => item.id !== card.id);
+    setCards(updated);
+  };
 
   return (
     <section className={styles.maker}>
       <div className={styles.container}>
-        <Editor cards={cards} addCard={addCard}/>
-        <Preview cards={cards}/>
+        <Editor
+          cards={cards}
+          addCard={addCard}
+          updateCard={updateCard}
+          deleteCard={deleteCard}
+        />
+        <Preview cards={cards} />
       </div>
     </section>
-  )
+  );
 }
