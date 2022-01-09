b739539f59ca60ac862cc5edee99721f2bc66f99
eunbiggabi <eunbiggabi84@gmail.com>
2022-01-09 11:50:35 +1000

    add getstyles theme to search card

--- src/pages/search/search_info/search_card/search_card.jsx
+++ src/pages/search/search_info/search_card/search_card.jsx
@@ -5,7 +5,7 @@ export default function SearchCard({card}) {
   const {name, company, title, email, message, theme, fileName, fileURL} = card;
   const url = fileURL || DEFAULT_IMAGE;
   return (
-    <li className={`${styles.card}`}>
+    <li className={`${styles.card} ${getStyles(theme)}`}>
       <img className={styles.avatar} src={url} alt="profile" />
       <div className={styles.info}>
         <h1 className={styles.name}>{name}</h1>
@@ -16,4 +16,15 @@ export default function SearchCard({card}) {
       </div>
     </li>
   )
+}
+
+function getStyles(theme) {
+  switch (theme) {
+    case 'dark':
+      return styles.dark;
+    case 'light':
+      return styles.light;
+    case 'colorful':
+      return styles.colorful;
+  }
 }
