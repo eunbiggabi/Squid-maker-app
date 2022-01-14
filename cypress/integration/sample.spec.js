d17caa4e199409a1298d161fd47580c29c0e8585
eunbiggabi <eunbiggabi84@gmail.com>
2022-01-14 20:44:11 +1000

    install cypress

---
+++ cypress/integration/sample.spec.js
@@ -0,0 +1,22 @@
+// sample.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+
+describe('My First Test', () => {
+    beforeEach(() => {
+      // Cypress starts out with a blank slate for each test
+      // so we must tell it to visit our website with the `cy.visit()` command.
+      // Since we want to visit the same URL at the start of all our tests,
+      // we include it in our beforeEach function so that it runs before each test
+      cy.visit('https://epic-curie-8e15c5.netlify.app')
+  })
+
+  it("Link Test", () => {
+    // cy.get('.home_home_maker__WpMCR').click()
+    cy.get('.home_info')
+  })
+})
